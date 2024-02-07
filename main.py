ddd7320195cd02a8ed33d311a7a9cacc3b7276d3
Lawrence Guo <775569157@qq.com>
2024-02-07 18:57:47 +0800

    格式化代码

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import sounddevice as sd
 import soundfile as sf
 from pynput import keyboard
 
-
 # 全局变量
 option_presses = 0  # 跟踪Option键按下的次数
 recording = False  # 是否正在录音
@@ -82,18 +81,20 @@ def transcribe_audio(filename):
 
 import pyperclip
 
+
 def copy_to_clipboard(text):
     # 使用pyperclip将文本复制到剪贴板
     pyperclip.copy(text)
     print("文本已复制到剪贴板。")
 
+
 import pyautogui
+
 
 def paste():
     # 模拟按下并释放'ctrl+v'（Windows/Linux）或'command+v'（Mac）来执行粘贴操作
     # pyautogui.hotkey('ctrl', 'v')  # 对于Windows和Linux
     pyautogui.hotkey('command', 'v')  # 对于Mac
-
 
 
 # 使用线程来执行录音和停止录音操作，避免阻塞键盘监听器
@@ -103,5 +104,3 @@ import threading
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 listener.join()
-
-# test
